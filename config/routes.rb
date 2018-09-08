e82b93553d89b3e52bf61e4fb8ce2b546075d090
Tak <programming.training2018@gmail.com>
2018-09-08 08:24:41 +0000

    user list added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'toppages/index'
+
+  root to: 'toppages#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-      root to: 'tasks#index'
-      resources :tasks
+  # root to: 'tasks#index'
+  get 'signup', to: 'users#new'
+  resources :tasks
+  resources :users
   
 end
